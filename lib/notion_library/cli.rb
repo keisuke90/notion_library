77c9cd7e131066925d2bdcf85414f66e36583cd2
KeisukeKuwahara <kuwa.90k@gmail.com>
2024-05-12 22:02:42 +0900

    initコマンド

--- lib/notion_library/cli.rb
+++ lib/notion_library/cli.rb
@@ -1,11 +1,24 @@
 require "notion_library"
 require "thor"
+require "dotenv"
 
 module NotionLibrary
   class CLI < Thor
     desc "hello", "Prints 'Hello World!'"
     def hello
       puts "Hello World!"
+    end
+
+    desc "init", "Initializes Secret Key"
+    def init_notion_library
+      Dotenv.load
+      puts "Please enter your Rakuten App ID\nIf you do not want to change it, leave it blank and press enter.\nRakuten Web Service App ID: #{ENV["RAKUTEN_APP_ID"]}"
+      rakuten_app_id = ask("Please enter new Rakuten APP ID:")
+      rakuten_app_id = ENV["RAKUTEN_APP_ID"] if rakuten_app_id.empty?
+
+      File.open(File.join(__dir__, "../../.env"), "w") do |file|
+        file.puts "RAKUTEN_APP_ID=#{rakuten_app_id}"
+      end
     end
   end
 end
