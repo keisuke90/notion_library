12c8278851915e8681f25854a364c4dce1d94665
KeisukeKuwahara <kuwa.90k@gmail.com>
2024-05-19 23:01:50 +0900

    ログイン処理修正

--- lib/notion_library/kindle/client.rb
+++ lib/notion_library/kindle/client.rb
@@ -26,15 +26,13 @@ module Kindle
       form.password = password
       res = mechanize_client.submit(form)
 
-      if res.search("#auth-captcha-image").any?
-        resolution_url = res.link_with(text: /See a new challenge/).resolved_uri.to_s
-        raise CaptchaError, "Received a CAPTCHA while attempting to sign in to your Amazon account. You will need to resolve this manually at #{resolution_url}"
-      elsif res.search("#message_error > p").any?
-        amazon_error = res.search("#message_error > p").children.first.to_s.strip
-        raise AuthenticationError, "Unable to sign in, received error: '#{amazon_error}'"
-      else
+      if res.search(".kp-notebook-title").any?
         @logged_in = true
+      else
+        raise AuthenticationError, "ログインに失敗しました。"
       end
+    rescue StandardError => e
+      puts e.message
     end
 
     def mechanize_client
